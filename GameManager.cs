d8d7573ec6c0d87c58a889e17632c463d9447fe1
Dyooni <kdy90518@naver.com>
2025-01-14 02:04:12 +0900

    second commit

--- GameManager.cs
+++ GameManager.cs
@@ -1,6 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
 
+    public float jelatine;
+    public float gold;
+    float jelatineValue;
+    float goldValue;
+
+    public Text jelatineText;
+    public Text goldText;
+
+    public RuntimeAnimatorController[] levelAc;
+
+    void Awake()
+    {
+        instance = this;    
+    }
+
+    void Start()
+    {
+        jelatineValue = PlayerPrefs.GetFloat("Jelatine", 0);
+        goldValue = PlayerPrefs.GetFloat("Gold", 0);
+    }
+
+    void LateUpdate()
+    {
+        jelatineValue = Mathf.SmoothStep(jelatineValue, jelatine, 0.5f);
+        goldValue = Mathf.SmoothStep(goldValue, gold, 0.5f);
+
+        jelatineText.text = string.Format("{0:n0}", jelatineValue);
+        goldText.text = string.Format("{0:n0}", goldValue);
+
+        PlayerPrefs.SetFloat("Jelatine", jelatineValue);
+        PlayerPrefs.SetFloat("Gold", goldValue);
+    }
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = levelAc[level-1];
+    }
 }
--- Jelly.cs
+++ Jelly.cs
@@ -1,42 +1,48 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Jelly : MonoBehaviour
 {
     float currentTime = 0;
-    public float speedX;
-    public float speedY;
+    float speedX;
+    float speedY;
     bool isWalking = false;
-    public bool isLineOut = false;
+    bool isLineOut = false;
+    public int id;
+    public int level;
+    public float exp = 0;
 
     public Transform topLeft;
     public Transform bottomRight;
     public Vector3[] pointList;
     Vector3 nextPoint;
-    public Vector3 moveDir;
+    Vector3 moveDir;
 
     Animator anim;
     SpriteRenderer spriter;
+    Collider2D collJelly;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        collJelly = GetComponent<Collider2D>();
     }
 
     void Update()
     {
         currentTime += Time.deltaTime;
 
-        if (!isWalking && currentTime >= Random.Range(2, 5)) {
+        if (!isWalking && currentTime >= Random.Range(1.9f, 4.9f)) {
             StartWalking();
         }
-        else if (isWalking && currentTime >= Random.Range(2, 5)) {
+        else if (isWalking && currentTime >= Random.Range(2.9f, 4.9f)) {
             StopWalking();
         }
 
         CheckLineOut();
         FlipJelly();
+        AutoGetExp();
+        levelUp();
     }
 
     void FixedUpdate()
@@ -89,6 +95,40 @@ public class Jelly : MonoBehaviour
         }
         else if (isLineOut) {
             spriter.flipX = nextPoint.x - transform.position.x < 0;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        anim.SetTrigger("doTouch");
+        GetJelatine();
+        StopWalking();
+    }
+
+    void GetJelatine()
+    {
+        GameManager.instance.jelatine += (id + 1) * level;
+        Mathf.Min(GameManager.instance.jelatine, 999999999);
+
+        if (level < 3) {
+            exp++;
+        }
+    }
+
+    void AutoGetExp()
+    {
+        if (level < 3) {
+            exp += 1 * Time.deltaTime;
+        }
+    }
+
+    void levelUp()
+    {
+        int maxExp = level * 50;
+
+        if (exp >= maxExp) {
+            level++;
+            GameManager.instance.ChangeAc(anim, level);
         }
     }
 }
